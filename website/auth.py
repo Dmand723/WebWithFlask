dae8dd8122c54e97ff8c6e35307f42079440dd99
Dmand723 <dawson.simmons723@gmail.com>
2025-03-10 09:53:00 -0600

    Added added and deleting notes

--- website/auth.py
+++ website/auth.py
@@ -24,7 +24,7 @@ def login():
                flash('Incorrect password, try again',category='error')
         else:
             flash('No user with that email',category='error')
-    return render_template('login.html')
+    return render_template('login.html',user=current_user)
 
 
 @auth.route('/sign-up',methods=['GET','POST'])
@@ -54,7 +54,7 @@ def signup():
             return redirect(url_for('views.home'))
             
 
-    return render_template('sign_up.html')
+    return render_template('sign_up.html',user=current_user)
 
 @auth.route('/logout')
 @login_required
--- website/veiws.py
+++ website/veiws.py
@@ -1,7 +1,35 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template, request,flash, jsonify
+from flask_login import  login_required , current_user
+from .models import Note
+from . import db
+import json
 
 veiws = Blueprint('views',__name__)
 
-@veiws.route('/')
+@veiws.route('/',methods=['GET','POST'])
+@login_required
 def home():
-    return render_template('home.html')
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash("Note is too short","error")
+        else:
+            newNote = Note(data=note,user_id=current_user.id)
+            db.session.add(newNote)
+            db.session.commit()
+            fla = flash("Note Added","success")
+    return render_template('home.html',user=current_user)
+
+
+@veiws.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            flash('Note deleted','warning')
+    return jsonify({})
